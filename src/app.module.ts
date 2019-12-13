f0f314e0752bd443d20d2f5a89710039c6d4dfbc
gdrpAPeng <522731323@qq.com>
2019-12-13 17:35:10 +0800

    添加 user 结构

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessageModule } from './message/message.module';
+import { UserModule } from './user/user.module';
 
 
 @Module({
-  imports: [MessageModule],
+  imports: [MessageModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
